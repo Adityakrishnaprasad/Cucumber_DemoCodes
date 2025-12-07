aa6e5acbb438f19c3f17d637723e782cdf9c1252
Adityarockyroy <adityarockyroy136@gmail.com>
2025-12-07 11:46:32 +0530

    Basic scripts of BDD Framework

--- src/test/java/cucmberOptions/TestNGRunner.java
+++ src/test/java/cucmberOptions/TestNGRunner.java
@@ -3,7 +3,7 @@ package cucmberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/featureFile/product.feature", glue="stepDefinitions",monochrome=true)
+@CucumberOptions(features="src/test/java/featureFile/product.feature", glue="stepDefinitions",monochrome=true, tags="not @RegressionTest")
 public class TestNGRunner extends AbstractTestNGCucumberTests { 
 	//C:\Users\adity\eclipse-workspace\Cucumber_DemoCodes\src\test\java\featurefile\product.feature tags="not @RegressionTest"
 }
