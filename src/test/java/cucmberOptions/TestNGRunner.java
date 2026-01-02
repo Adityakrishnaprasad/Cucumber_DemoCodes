9466a00398f562c38c64eb5c64e9fdf6a8f0d24f
Adityarockyroy <adityarockyroy136@gmail.com>
2026-01-02 21:17:21 +0530

    UpdatedTestRunnerClass

--- src/test/java/cucmberOptions/TestNGRunner.java
+++ src/test/java/cucmberOptions/TestNGRunner.java
@@ -3,7 +3,7 @@ package cucmberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/featureFile/product.feature", glue="stepDefinitions",monochrome=true, tags="not @RegressionTest")
+@CucumberOptions(features="src/test/java/featureFile/login.feature", glue="stepDefinitions",monochrome=true, tags="not @SanityTest")
 public class TestNGRunner extends AbstractTestNGCucumberTests { 
 	//C:\Users\adity\eclipse-workspace\Cucumber_DemoCodes\src\test\java\featurefile\product.feature tags="not @RegressionTest"
 }
